d40548cdf73114df306788c54bc6a0ec2949dec4
Bryan_G <40415998+BryGo1995@users.noreply.github.com>
2025-05-14 14:49:55 -0600

    Center the title text on the main menu

--- src/ui/menu.lua
+++ src/ui/menu.lua
@@ -5,9 +5,9 @@ function Menu.new()
     local self = setmetatable({}, Menu)
     self.title = {
         text = "Diced",
-        font = love.graphics.newFont(48),
+        font = love.graphics.newFont(144),
         x = love.graphics.getWidth()/2,
-        y = love.graphics.getHeight()/2
+        y = love.graphics.getHeight()/3,
     }
 
     return self
@@ -18,8 +18,11 @@ function Menu:update()
 end
 
 function Menu:draw()
+    -- Draw the title text TODO: put in its own function
     love.graphics.setFont(self.title.font)
-    love.graphics.print(self.title.text, self.title.x, self.title.y)
+    local xoffset = self.title.font:getWidth(self.title.text)/2
+    local yoffset = self.title.font:getHeight(self.title.text)/2
+    love.graphics.print(self.title.text, self.title.x - xoffset, self.title.y - yoffset)
 end
 
 return Menu
