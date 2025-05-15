0347a4a27384a35c291832fe7cdf28188f252052
Bryan_G <40415998+BryGo1995@users.noreply.github.com>
2025-05-14 19:42:11 -0600

    Reduce calls to gameState:getState()

--- src/main.lua
+++ src/main.lua
@@ -29,13 +29,14 @@ function love.draw()
 end
 
 function love.mousepressed(x, y, button)
-    if gameState:getState() == states.MAIN_MENU then
+    local currentState = gameState:getState()
+    if currentState == states.MAIN_MENU then
         print("Main Menu")
         gameState:changeState(states.GAME_LOOP)
-    elseif gameState:getState() == states.GAME_LOOP then
+    elseif currentState == states.GAME_LOOP then
         print("Game")
         gameState:changeState(states.GAME_OVER)
-    elseif gameState:getState() == states.GAME_OVER then
+    elseif currentState == states.GAME_OVER then
         print("Game Over")
         gameState:changeState(states.MAIN_MENU)
     else
