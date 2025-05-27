d3a42f47c761b63976a9a9d8500a33084d69b45d
Bryan_G <40415998+BryGo1995@users.noreply.github.com>
2025-05-27 15:49:36 -0600

    Add the roll dice button to the main game loop

--- main.lua
+++ main.lua
@@ -26,6 +26,7 @@ end
 
 function love.update(dt)
     menu:update(dt)
+    gameLoop:update(dt)
     gameOver:update(dt)
 end
 
--- src/ui/gameloop.lua
+++ src/ui/gameloop.lua
@@ -2,9 +2,14 @@ local GameLoop = {}
 GameLoop.__index = GameLoop
 
 local Dice = require("src/ui/dice")
+local Button = require("src/ui/button")
 
 gameWindow = {height = 800, width = 1400}
 verticalCells = 5
+
+local buttons = {}
+local buttonWidth = 200
+local buttonHeight = 60
 
 function GameLoop.new(numOfDice)
     local self = setmetatable({}, GameLoop)
@@ -15,21 +20,47 @@ function GameLoop.new(numOfDice)
     return self
 end
 
+function GameLoop:initializeButtons()
+    buttons = {
+        Button.new(
+            "Roll Dice",
+            love.graphics.getWidth()/2 - buttonWidth/2,
+            love.graphics.getHeight()*0.9,
+            buttonWidth,
+            buttonHeight,
+            {
+                font = love.graphics.setNewFont(30),
+                onClick = function()
+                    print("Roll button pressed")
+                end
+            }
+        )
+    }
+end
+
 function GameLoop:init()
     local pos = setDicePositions(self.numOfDice)
     for i = 1, self.numOfDice do
         self.dice[i] = Dice.new(6)
         self.dice[i]:init(pos[i])        
     end
+
+    self:initializeButtons()
 end
 
 function GameLoop:update(dt)
-
+    for _, b in ipairs(buttons) do
+        b:update()
+    end
 end
 
 function GameLoop:draw()
     for i = 1, self.numOfDice do
         self.dice[i]:draw()
+    end
+
+    for _, b in ipairs(buttons) do
+        b:draw()
     end
 end
 
