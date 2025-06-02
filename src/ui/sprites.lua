3a8ae404bd54d3ec32839f9d04b9d6130878ea07
Bryan_G <40415998+BryGo1995@users.noreply.github.com>
2025-06-01 20:40:37 -0600

    Load the 8 sided dice sprites

--- src/ui/sprites.lua
+++ src/ui/sprites.lua
@@ -10,4 +10,14 @@ sprites.d6_5 = love.graphics.newImage("assets/sprites/d6-w/D6-5.png")
 sprites.d6_6 = love.graphics.newImage("assets/sprites/d6-w/D6-6.png")
 sprites.d6_blank = love.graphics.newImage("assets/sprites/d6-w/D6-0.png")
 
+sprites.d8_1 = love.graphics.newImage("assets/sprites/d8-gy/D8-1.png")
+sprites.d8_2 = love.graphics.newImage("assets/sprites/d8-gy/D8-2.png")
+sprites.d8_3 = love.graphics.newImage("assets/sprites/d8-gy/D8-3.png")
+sprites.d8_4 = love.graphics.newImage("assets/sprites/d8-gy/D8-4.png")
+sprites.d8_5 = love.graphics.newImage("assets/sprites/d8-gy/D8-5.png")
+sprites.d8_6 = love.graphics.newImage("assets/sprites/d8-gy/D8-6.png")
+sprites.d8_7 = love.graphics.newImage("assets/sprites/d8-gy/D8-7.png")
+sprites.d8_8 = love.graphics.newImage("assets/sprites/d8-gy/D8-8.png")
+sprites.d8_blank = love.graphics.newImage("assets/sprites/d8-gy/D8-0.png")
+
 return sprites
