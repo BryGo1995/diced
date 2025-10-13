d179d8fefaf4a7eda40d536efa25ab8be6847bbc
Bryan_G <40415998+BryGo1995@users.noreply.github.com>
2025-10-13 17:21:47 -0600

    Remove old exitIcon code from stats class and add an active state flag

--- src/ui/stats.lua
+++ src/ui/stats.lua
@@ -7,22 +7,14 @@ local Button = require("src/ui/button")
 function Stats.new()
     local self = setmetatable({}, Stats)
 
-    -- Initialize with default values, will be updated later
+    self.active = false
     self.window = {
         sprite = Sprites.statsWindowHanging,
         scale = 3.6,
         x = 100,
         y = 100,
         width = 400,
-        height = 300
-    }
-    self.exitIcon = {
-        sprite = Sprites.exitIconSprite,
-        scale = 3.6,
-        x = 100,
-        y = 100,
-        color = {1, 1, 1},
-        hoveredColor = {0.5, 0.5, 0.5}
+        height = 300,
     }
     self.buttons = {}
 
@@ -35,9 +27,6 @@ function Stats:init()
     self.window.height = love.graphics.getHeight()*0.5
     self.window.x = love.graphics.getWidth()/2 - self.window.sprite:getWidth()*self.window.scale/2
     self.window.y = love.graphics.getHeight()/2 - self.window.sprite:getHeight()*self.window.scale/2
-
-    self.exitIcon.x = self.window.x + self.window.width + 20
-    self.exitIcon.y = self.window.y + 225
 
     self.text = {
         x = self.window.x + 50,
@@ -53,21 +42,25 @@ function Stats:update()
 end
 
 function Stats:onClick(x, y)
-    for _, b in ipairs(self.buttons) do
-        if b:isPointInside(x, y) then
-            b:onClick()
+    if self.active == true then
+        for _, b in ipairs(self.buttons) do
+            if b:isPointInside(x, y) then
+                b:onClick()
+            end
         end
     end
 end
 
 function Stats:displayStats()
-    love.graphics.setBackgroundColor(0, 0, 0)
-    love.graphics.draw(self.window.sprite, self.window.x, self.window.y, 0, self.window.scale, self.window.scale)
-    love.graphics.setColor(0.5, 0.5, 0.5)
-    for _, b in ipairs(self.buttons) do
-        b:draw()
+    if self.active then
+        love.graphics.setBackgroundColor(0, 0, 0)
+        love.graphics.draw(self.window.sprite, self.window.x, self.window.y, 0, self.window.scale, self.window.scale)
+        love.graphics.setColor(0.5, 0.5, 0.5)
+        for _, b in ipairs(self.buttons) do
+            b:draw()
+        end
+        love.graphics.setColor(1, 1, 1)
     end
-    love.graphics.setColor(1, 1, 1)
 end
 
 function Stats:initializeButtons()
@@ -81,6 +74,7 @@ function Stats:initializeButtons()
                 spriteScaler = 3.6,
                 onClick = function()
                     print("Exit button clicked")
+                    self.active = false
                 end
             }
         )
